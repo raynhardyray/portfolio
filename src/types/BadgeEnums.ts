0b5c418666f1cb5184b542647f7308b8b823d6bb
Raynhard Anthony Yray <raynhardanthonyyray2001@gmail.com>
2025-04-07 12:48:21 +0800

    added Badge and sections folder

---
+++ src/types/BadgeEnums.ts
@@ -0,0 +1,16 @@
+export enum FrontEndSkills {
+    ReactJS = "ReactJS",
+    HTML = "HTML",
+    CSS = "CSS",
+    Tailwind = "Tailwind CSS",
+    ReactNative = "React Native",
+    DaisyUI = "DaisyUI",
+    Astro = "Astro",
+}
+
+export enum ProgrammingSkills {
+    Java = "Java",
+    Python = "Python",
+    JavaScript = "JavaScript",
+    TypeScript = "TypeScript",
+}
