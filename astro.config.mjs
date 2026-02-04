092041e24c2a0a7e10d0c786f4d08efcc2e10709
Raynhard Anthony Yray <raynhardanthonyyray2001@gmail.com>
2026-02-04 11:36:03 +0800

    removed tailwind, updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
   integrations: [icon()],
-  site: 'https://raynhardyray.github.io',
-  outDir: './dist',
-  build: {
-    assetsPrefix: './'
-  }
+  site: "https://raynhardyray.github.io",
+  outDir: "./dist",
 });
+
